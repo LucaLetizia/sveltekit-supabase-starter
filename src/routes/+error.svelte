3fe2bd090931e5222e94bb878e1472860d25f48d
lucaletizia <lucaletizia97@gmail.com>
2024-02-24 17:59:51 +0000

    Run prettier + add missing session checks

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import type { PageData } from './$types';
-  import { page } from '$app/stores';
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
 
-  export let data: PageData;
+	export let data: PageData;
 </script>
 
 <div class="h-screen">
-  <h1>{$page?.error?.message}</h1>
+	<h1>{$page?.error?.message}</h1>
 </div>
